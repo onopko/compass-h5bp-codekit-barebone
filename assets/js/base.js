04527d3e827237d7388e000d4bf7331ac638098c
ONO TAKEHIKO <ono@aguije.jp>
2014-05-19 13:55:58 +0900

    Optimize JavaScript Files.

---
+++ assets/js/base.js
@@ -0,0 +1,3 @@
+//@codekit-append "vendor/ga_social_tracking.js"
+//@codekit-append "vendor/cssua.min.js"
+//@codekit-append "_plugins.js"
--- assets/js/core.js
+++
@@ -1,5 +0,0 @@
-//@codekit-append "vendor/ga_social_tracking.js"
-//@codekit-append "vendor/cssua.min.js"
-//@codekit-append "_plugins.js"
-//@codekit-append "_common.js"
-
