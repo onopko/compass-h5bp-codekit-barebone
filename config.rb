feeaea16419f69dc422a37f90bd6ef71d7e679c2
onopko <me@onotakehiko.com>
2015-03-16 15:57:38 +0900

    config.rb から compass-h5bp の require 宣言を削除
    bower管理下としたため不要。

--- config.rb
+++ config.rb
@@ -1,6 +1,3 @@
-require 'compass-h5bp'
-# Require any additional compass plugins here.
-
 # Set this to the root of your project when deployed:
 http_path       = "/"
 css_dir         = "assets/css"
