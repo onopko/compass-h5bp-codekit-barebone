f95cbe9d2c2c2ead08f6047416365ab0b185165e
onopko <me@onotakehiko.com>
2015-03-16 15:55:23 +0900

    httpimagespath, relative_assets を設定

--- config.rb
+++ config.rb
@@ -2,22 +2,24 @@ require 'compass-h5bp'
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = "/"
-css_dir = "assets/css"
-sass_dir = "assets/scss"
-images_dir = "assets/images"
+http_path       = "/"
+css_dir         = "assets/css"
+sass_dir        = "assets/scss"
+images_dir      = "assets/images"
 javascripts_dir = "assets/js"
-fonts_dir = "assets/fonts"
+fonts_dir       = "assets/fonts"
+httpimagespath  = "/assets/images"
 
-output_style = :compressed
+output_style    = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = false
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
 color_output = false
 
 preferred_syntax = :scss
+
 
 # sass_options = { :debug_info => true }
