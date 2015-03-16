90d6e939e204fa682fd2164c8cc91c9313cdfbcd
onopko <me@onotakehiko.com>
2015-03-16 15:08:28 +0900

    console.log() の定義を先頭へ移動

--- assets/js/plugins.js
+++ assets/js/plugins.js
@@ -1,28 +1,28 @@
-//@codekit-append "vendor/ga_social_tracking.js"
-//@codekit-append "../../_vendor/cssuseragent/cssua.js"
-//@codekit-append "../../_vendor/underscore/underscore.js"
-
 // Avoid `console` errors in browsers that lack a console.
 (function() {
-    var method;
-    var noop = function () {};
-    var methods = [
-        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
-        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
-        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
-        'timeStamp', 'trace', 'warn'
-    ];
-    var length = methods.length;
-    var console = (window.console = window.console || {});
+	var method;
+	var noop = function () {};
+	var methods = [
+		'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+		'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+		'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+		'timeStamp', 'trace', 'warn'
+	];
+	var length = methods.length;
+	var console = (window.console = window.console || {});
 
-    while (length--) {
-        method = methods[length];
+	while (length--) {
+		method = methods[length];
 
-        // Only stub undefined methods.
-        if (!console[method]) {
-            console[method] = noop;
-        }
-    }
+		// Only stub undefined methods.
+		if (!console[method]) {
+			console[method] = noop;
+		}
+	}
 }());
 
 // Place any jQuery/helper plugins in here.
+
+//@codekit-append "vendor/ga_social_tracking.js"
+//@codekit-append "../../_vendor/cssuseragent/cssua.js"
+//@codekit-append "../../_vendor/underscore/underscore.js"
